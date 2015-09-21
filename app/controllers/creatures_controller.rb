dcc8f59d3fa495aeab81ba37ca1e1f3e3fc16a92
Marcel Degas <marcel.degas@gmail.com>
2015-09-21 12:17:38 -0700

    finished the crud actions

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,51 @@
+class CreaturesController < ApplicationController
+	def index
+		@creatures = Creature.all
+		render :index
+	end
+
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	def create
+		new_creature = params.require(:creature).permit(:name, :description)
+		Creature.create(new_creature)
+		# wtf?? why is this line not working?
+		redirect_to "/creatures/#{creature.id}"
+		# wtf?? why is this line not working?
+	end
+
+	def show
+        id = params[:id]
+        @creature = Creature.find(id)
+        render :show
+    end
+
+    def edit
+        id = params[:id]
+        @creature = Creature.find(id)
+        render :edit
+    end
+
+    def update
+        creature_id = params[:id]
+        creature = Creature.find(Creature_id)
+
+        # get updated data
+        updated_attributes = params.require(:creatue).permit(:name, :description)
+        # update the creature
+        creature.update_attributes(updated_attributes)
+
+        #redirect to show
+        redirect_to "/creatures/#{creature_id}"
+    end
+
+    def destroy
+        id = params[:id]
+        creature = Creature.find(id)
+        creature.destroy
+        redirect_to "/creatures"
+    end
+end
